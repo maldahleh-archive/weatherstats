39dc7f0f953fe7e29b2201bbeac170f01d4dde3b
Mohammed Al-Dahleh <maldahleh@gmail.com>
2020-02-13 11:11:02 -0500

    Data point selection

--- main.go
+++ main.go
@@ -1,20 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
 )
 
+const title = "WeatherStats"
+
+func createDataPointSelector() (*widget.Select, *widget.Box) {
+	selector := widget.NewSelect([]string{
+		"maxtemp",
+		"mintemp",
+		"meantemp",
+		"rain",
+		"snow",
+		"snowgrnd",
+		"precip",
+		"maxgust",
+	}, func(dataPoint string) {})
+
+	selector.SetSelected("maxtemp")
+	return selector, widget.NewHBox(
+			widget.NewLabel("Select a Data Point:"),
+			selector,
+		)
+}
+
 func main() {
 	a := app.New()
 
-	w := a.NewWindow("WeatherStats")
+	dataPointSelector, dataPointBox := createDataPointSelector()
+
+	w := a.NewWindow(title)
 	w.SetContent(widget.NewVBox(
-		widget.NewLabel("WeatherStats"),
+		widget.NewLabel(title),
+		dataPointBox,
 		widget.NewButton("Quit", func() {
+			fmt.Println(dataPointSelector.Selected)
 			a.Quit()
 		}),
 	))
 
+	w.SetPadded(true)
+	w.SetFullScreen(true)
 	w.ShowAndRun()
 }
